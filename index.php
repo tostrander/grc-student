0c00771625f0ca9b3affb7caf92b0abf9a76bd81
Tina Ostrander <tostrander@greenriver.edu>
2019-05-20 16:34:42 -0400

    Add composer

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function() {
+    echo "GRC Students";
+});
+
+//Run fat free
+$f3->run();
