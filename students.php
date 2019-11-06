3f95c88d1b7eedbd868dd50180bc5a1c119cdf15
Tina Ostrander <tina.j.ostrander@gmail.com>
2019-11-06 08:24:50 -0800

    Add students.php via upload

---
+++ students.php
@@ -0,0 +1,62 @@
+<?php
+    /** 305/students.php reads students from a database
+     *  into a data table
+     *  Nov 4, 2019
+     */
+
+    //Turn on error reporting -- this is critical!
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GRC Student Summary</title>
+</head>
+<body>
+
+    <h3>Student Summary</h3>
+
+    <?php
+    //Connect to db
+    $username = 'tostrand_grcuser';
+    $password = '*k*uyu*]N;,c';
+    $hostname = 'localhost';
+    $database = 'tostrand_grc';
+
+    $cnxn = mysqli_connect($hostname, $username, $password, $database);
+
+    //Test connection
+    if ($cnxn) {
+        echo "<p>Connected!</p>";
+    } else {
+        echo mysqli_connect_error();
+    }
+
+    //Define the query
+    $sql = 'SELECT s.sid, s.first AS student_first, s.last AS student_last, s.advisor, a.advisor_id, 
+            a.advisor_first, a.advisor_last 
+            FROM student s, advisor a 
+            WHERE a.advisor_id = s.advisor';
+
+    //Send the query to the database
+    $result = mysqli_query($cnxn, $sql);
+    //var_dump($result);
+
+    //Print the results
+    while ($row = mysqli_fetch_assoc($result)) {
+        $sid = $row['sid'];
+        $sFirst = $row['student_first'];
+        $sLast = $row['student_last'];
+        $aFirst = $row['advisor_first'];
+        $aLast = $row['advisor_last'];
+
+        echo "$sid - $sFirst $sLast, $aFirst $aLast<br>";
+    }
+
+?>
+
+</body>
+</html>
